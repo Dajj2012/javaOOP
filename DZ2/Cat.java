8153db12608d4aaac54fa8dc098d452dfe8111bf
Alexandr <dajj2012@mail.ru>
2023-05-02 10:33:49 +0300

    2DZ

---
+++ DZ2/Cat.java
@@ -0,0 +1,27 @@
+package DZ2;
+
+public class Cat {
+    private String name;
+    private int appetite;
+
+    private int satiety;
+
+    public int getAppetite() {
+        return appetite;
+    }
+
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+    }
+    public void eat(int satiety) {
+        this.satiety = satiety;
+        if(satiety < 0){
+            System.out.printf("%s: 'Маловато будет, не буду есть!'\n", name);
+        }
+        else {
+            System.out.printf("%s: 'Ништяк, поел!'\n", name);
+        }
+    }
+}
+
