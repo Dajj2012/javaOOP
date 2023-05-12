833d30ffa07a00ae4d620fcf567b610dce020042
Alexandr <dajj2012@mail.ru>
2023-05-12 07:53:56 +0300

    DZ5

---
+++ DZ5/presenters/View.java
@@ -0,0 +1,27 @@
+package DZ5.presenters;
+
+import DZ5.models.Table;
+
+import java.util.Collection;
+
+public interface View {
+
+    /**
+     * Отобразить список всех столиков
+     * 
+     * @param tables список столиков
+     */
+    void showTables(Collection<Table> tables);
+
+    void showReservationStatus(int reservationNo);
+
+    void showEditReservationStatus(int oldReservationNo, int reservationNo);
+
+    /**
+     * Установить нового наблюдателя
+     * 
+     * @param observer наблюдатель (презентер)
+     */
+    void setObserver(ViewObserver observer);
+
+}
